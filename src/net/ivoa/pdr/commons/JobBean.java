c002c372833f5ac905da190b49466ffc76c55125
zwolf <zwolf@lerma-m-cz.obspm.fr>
2015-11-25 14:16:44 +0100

    Modyfing JobBean for including the jobNickName and the gridId

--- src/net/ivoa/pdr/commons/JobBean.java
+++ src/net/ivoa/pdr/commons/JobBean.java
@@ -5,121 +5,154 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * @author Carlo Maria Zwolf
- * Observatoire de Paris
- * LERMA
+ * @author Carlo Maria Zwolf Observatoire de Paris LERMA
  */
 
-public class JobBean implements Serializable{
-
-
+public class JobBean implements Serializable {
 
 	public Integer getIdJob() {
 		return IdJob;
 	}
+
 	public Integer getIdService() {
 		return IdService;
 	}
+
 	public Map<Integer, String> getUserToNotify() {
 		return userToNotify;
 	}
-	
+
 	public Map<String, String> getJobConfiguration() {
 		return jobConfiguration;
 	}
-	public Map<String,String> getJobResults() {
+
+	public Map<String, String> getJobResults() {
 		return jobResults;
 	}
+
 	public List<String> getJobErrors() {
 		return jobErrors;
 	}
+
 	public boolean isJobProcessed() {
 		return jobProcessed;
 	}
+
 	public boolean isJobfinished() {
 		return jobfinished;
 	}
+
 	public boolean isJobValid() {
 		return jobValid;
 	}
+
 	public String getDemandDate() {
 		return demandDate;
 	}
+
 	public String getProcessingDate() {
 		return processingDate;
 	}
+
 	public String getFinishingDate() {
 		return finishingDate;
 	}
+
 	public void setIdJob(Integer idJob) {
 		IdJob = idJob;
 	}
+
 	public void setIdService(Integer idService) {
 		IdService = idService;
 	}
+
 	public void setUserToNotify(Map<Integer, String> userToNotify) {
 		this.userToNotify = userToNotify;
 	}
-	
+
 	public void setJobConfiguration(Map<String, String> jobConfiguration) {
 		this.jobConfiguration = jobConfiguration;
 	}
-	public void setJobResults(Map<String,String> jobResults) {
+
+	public void setJobResults(Map<String, String> jobResults) {
 		this.jobResults = jobResults;
 	}
+
 	public void setJobErrors(List<String> jobErrors) {
 		this.jobErrors = jobErrors;
 	}
+
 	public void setJobProcessed(boolean jobProcessed) {
 		this.jobProcessed = jobProcessed;
 	}
+
 	public void setJobfinished(boolean jobfinished) {
 		this.jobfinished = jobfinished;
 	}
+
 	public void setJobValid(boolean jobValid) {
 		this.jobValid = jobValid;
 	}
+
 	public void setDemandDate(String demandDate) {
 		this.demandDate = demandDate;
 	}
+
 	public void setProcessingDate(String processingDate) {
 		this.processingDate = processingDate;
 	}
+
 	public void setFinishingDate(String finishingDate) {
 		this.finishingDate = finishingDate;
 	}
+
 	public void setUserAskedThisJob(List<List<String>> userAskedThisJob) {
 		this.userAskedThisJob = userAskedThisJob;
 	}
+
 	public List<List<String>> getUserAskedThisJob() {
 		return userAskedThisJob;
 	}
+
 	public String getErrors() {
 		return errors;
 	}
+
 	public void setErrors(String errors) {
 		this.errors = errors;
 	}
+
 	public String getPhase() {
 		return phase;
 	}
+
 	public void setPhase(String phase) {
 		this.phase = phase;
 	}
+
+	public boolean isHasError() {
+		return hasError;
+	}
+
+	public void setHasError(boolean hasError) {
+		this.hasError = hasError;
+	}
+
 	private Integer IdJob;
 	private Integer IdService;
-	private Map<Integer,String> userToNotify;
+	private Map<Integer, String> userToNotify;
 	private List<List<String>> userAskedThisJob;
-	private Map<String,String> jobConfiguration;
-	private Map<String,String> jobResults;
+	private Map<String, String> jobConfiguration;
+	private Map<String, String> jobResults;
 	private List<String> jobErrors;
 	private boolean jobProcessed;
 	private boolean jobfinished;
 	private boolean jobValid;
+	private boolean hasError;
 	private String demandDate;
 	private String processingDate;
 	private String finishingDate;
 	private String errors;
 	private String phase;
-	
+
 }
